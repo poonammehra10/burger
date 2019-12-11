73be9fe930ce635c84348ff41454d9bd13bdbefb
Poonam Mehra <52021588+poonammehra10@users.noreply.github.com>
2019-12-10 17:41:24 -0800

    fixed

---
+++ db/seed.sql
@@ -0,0 +1,7 @@
+
+USE `burgers_db`;
+
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES (`Angus`, falses, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES (`Salmon`, false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES (`Mushroom`, false, CURRENT_TIMESTAMP);
+
